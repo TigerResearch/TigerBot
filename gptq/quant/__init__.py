a6f765cb76aec81a85b3073798573609a22bf3ac
Vivicai1005 <wei.cai@algolet.com>
2023-05-31 10:01:10 +0800

    edit quant init

--- gptq/quant/__init__.py
+++ gptq/quant/__init__.py
@@ -1,2 +1,5 @@
 from .quantizer import Quantizer
+from .fused_attn import make_quant_attn
+from .fused_mlp import make_fused_mlp, autotune_warmup_fused
 from .quant_linear import QuantLinear, make_quant_linear, autotune_warmup_linear
+from .triton_norm import make_quant_norm
