6752cecf60c6b677e67be7a23b3356c735e2be92
yechen <yechen@yes-macbook-pro>
2023-06-08 16:36:08 +0800

    infer pretrain

--- infer_pretrain.py
+++ infer_pretrain.py
@@ -1,17 +1,46 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
-import torch
-import fire
 import os
+
+import fire
+import torch
+import readline
+from accelerate import infer_auto_device_map, dispatch_model
+from accelerate.utils import get_balanced_memory
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
+tok_ins = "\n\n### Instruction:\n"
+tok_res = "\n\n### Response:\n"
+prompt_input = tok_ins + "{instruction}" + tok_res
+
+
+def get_model(model):
+    def skip(*args, **kwargs):
+        pass
+
+    torch.nn.init.kaiming_uniform_ = skip
+    torch.nn.init.uniform_ = skip
+    torch.nn.init.normal_ = skip
+    model = AutoModelForCausalLM.from_pretrained(model, torch_dtype=torch.float16)
+    return model
+
 
 def main(
-    model_path: str="TigerResearch/tigerbot-7b-base",
-    max_input_length: int=512,
-    max_generate_length: int=1024,
+        model_path: str,
+        max_input_length: int = 512,
+        max_generate_length: int = 1024,
 ):
     print(f"loading model: {model_path}...")
+
+    model = get_model(model_path)
+    max_memory = get_balanced_memory(model)
+    device_map = infer_auto_device_map(model, max_memory=max_memory,
+                                       no_split_module_classes=["BloomBlock"])
+    print("Using the following device map for the model:", device_map)
+    model = dispatch_model(model, device_map=device_map, offload_buffers=True)
+
+    device = torch.cuda.current_device()
+
     tokenizer = AutoTokenizer.from_pretrained(
         model_path,
         cache_dir=None,
@@ -19,16 +48,11 @@ def main(
         padding_side="left",
         truncation_side='left',
         padding=True,
-        truncation=True,
-        # use_fast=False,
+        truncation=True
     )
-    if tokenizer.model_max_length is None or tokenizer.model_max_length > 1024:
-        tokenizer.model_max_length = 1024
-    
-    model = AutoModelForCausalLM.from_pretrained(model_path, pad_token_id=tokenizer.pad_token_id, torch_dtype=torch.bfloat16)
-    device = torch.cuda.current_device()
-    model.to(device)
-    
+    if tokenizer.model_max_length is None or tokenizer.model_max_length > max_generate_length:
+        tokenizer.model_max_length = max_generate_length
+
     generation_kwargs = {
         "top_p": 0.95,
         "temperature": 0.8,
@@ -37,7 +61,7 @@ def main(
         "pad_token_id": tokenizer.pad_token_id,
         "early_stopping": True,
         "no_repeat_ngram_size": 4,
-        }
+    }
 
     while True:
         raw_text = input("prompt(\"exit\" to end, \"clear\" to clear session) >>> ")
@@ -61,4 +85,3 @@ def main(
 
 if __name__ == "__main__":
     fire.Fire(main)
-
