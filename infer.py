e9dcb53f5eec75b697da2f589fdd28d96a90b1ce
fc <i4never@163.com>
2023-08-08 18:17:13 +0800

    [UPDATE] infer.py

--- infer.py
+++ infer.py
@@ -3,8 +3,6 @@ import os
 import fire
 import torch
 import readline
-from accelerate import infer_auto_device_map, dispatch_model
-from accelerate.utils import get_balanced_memory
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
@@ -14,17 +12,6 @@ tok_res = "\n\n### Response:\n"
 prompt_input = tok_ins + "{instruction}" + tok_res
 
 
-def get_model(model):
-    def skip(*args, **kwargs):
-        pass
-
-    torch.nn.init.kaiming_uniform_ = skip
-    torch.nn.init.uniform_ = skip
-    torch.nn.init.normal_ = skip
-    model = AutoModelForCausalLM.from_pretrained(model, torch_dtype=torch.float16)
-    return model
-
-
 def main(
         model_path: str,
         max_input_length: int = 512,
@@ -32,18 +19,12 @@ def main(
 ):
     print(f"loading model: {model_path}...")
 
-    model = get_model(model_path)
-    max_memory = get_balanced_memory(model)
-    device_map = infer_auto_device_map(model, max_memory=max_memory,
-                                       no_split_module_classes=["BloomBlock"])
-    print("Using the following device map for the model:", device_map)
-    model = dispatch_model(model, device_map=device_map, offload_buffers=True)
+    model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.bfloat16, device_map='auto')
 
     device = torch.cuda.current_device()
 
     tokenizer = AutoTokenizer.from_pretrained(
         model_path,
-        cache_dir=None,
         model_max_length=max_generate_length,
         padding_side="left",
         truncation_side='left',
@@ -60,7 +41,6 @@ def main(
         "eos_token_id": tokenizer.eos_token_id,
         "pad_token_id": tokenizer.pad_token_id,
         "early_stopping": True,
-        "no_repeat_ngram_size": 4,
     }
 
     sess_text = ""
