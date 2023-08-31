1095c41f013172f817f7ad629f07a0e4734d9381
wangyu1997 <wangyu19970819@qq.com>
2023-08-31 11:38:21 +0800

    fix bugs

--- opencompass/opencompass/models/__init__.py
+++ opencompass/opencompass/models/__init__.py
@@ -2,7 +2,7 @@ from .base import BaseModel, LMTemplateParser  # noqa
 from .base_api import APITemplateParser, BaseAPIModel  # noqa
 from .glm import GLM130B  # noqa: F401, F403
 from .huggingface import HuggingFace  # noqa: F401, F403
-from .huggingface import HuggingFaceCausalLM  # noqa: F401, F403
+from .huggingface import HuggingFaceCausalLM, GPTQCausalLM, ExllamaCausalLM
+  # noqa: F401, F403
 from .llama2 import Llama2Chat  # noqa: F401, F403
 from .openai_api import OpenAI  # noqa: F401
-from .tigerbot import Tigerbot, TigerbotAutoGPTQ, TigerbotCausalLM, TigerbotExllama
