755d578d2e3bdbb9a09b7f8e11313ca68c42f0c0
fc <i4never@163.com>
2023-09-20 13:51:04 +0800

    Support base model infer

--- infer.py
+++ infer.py
@@ -1,8 +1,11 @@
 import os
+import readline
 
 import fire
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM, GenerationConfig
+
+print(readline)
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
@@ -15,13 +18,16 @@ def main(
         model_path: str,
         max_input_length: int = 512,
         max_generate_length: int = 1024,
+        model_type: str = 'chat',
         use_flash_attn: bool = False
 ):
+    if model_type.lower() not in ['chat', 'base']:
+        raise ValueError(f"model_type must be one of ['chat', 'base'], got {model_type}")
     if use_flash_attn:
         from flash_attention import replace_attn_with_flash_attn
         replace_attn_with_flash_attn()
         print("using flash attention...")
-            
+
     print(f"loading model: {model_path}...")
 
     model = AutoModelForCausalLM.from_pretrained(model_path, torch_dtype=torch.bfloat16, device_map='auto')
@@ -59,12 +65,15 @@ def main(
 
         query_text = raw_text.strip()
         sess_text += tok_ins + query_text
-        input_text = prompt_input.format_map({'instruction': sess_text.split(tok_ins, 1)[1]})
+        if model_type == 'chat':
+            input_text = prompt_input.format_map({'instruction': sess_text.split(tok_ins, 1)[1]})
+        else:
+            input_text = query_text
         inputs = tokenizer(input_text, return_tensors='pt', truncation=True, max_length=max_input_length)
         inputs = {k: v.to(device) for k, v in inputs.items()}
         output = model.generate(**inputs, **generation_config.to_dict())
-        output_str = tokenizer.decode(output[0], skip_special_tokens=False, spaces_between_special_tokens=False)
-        answer = output_str.rsplit(tok_res, 1)[1].strip()
+        answer = tokenizer.decode(output[0][inputs['input_ids'].shape[1]:], skip_special_tokens=False,
+                                  spaces_between_special_tokens=False)
         if answer.endswith(tokenizer.eos_token):
             answer = answer.rsplit(tokenizer.eos_token, 1)[0].strip()
 
