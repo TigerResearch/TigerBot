e9d9d8ce7e2cd213d42561c2f972617d47ea68d0
Vivicai1005 <wei.cai@tigerobo.com>
2024-02-06 16:04:02 +0800

    for testing

--- other_infer/exllamav2_hf_infer.py
+++ other_infer/exllamav2_hf_infer.py
@@ -258,6 +258,7 @@ def main(
         inputs = {k: v.to(device) for k, v in inputs.items()}
         print('=' * 100)
         answer = ""
+        print(generation_config)
         for text in generate_stream(model, tokenizer, inputs['input_ids'], inputs['attention_mask'],
                                     generation_config=generation_config):
             print(text, end='', flush=True)
