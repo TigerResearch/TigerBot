2cbce7419b3183ccef709ca28e8fcb7ebf03cbb9
Vivicai1005 <wei.cai@tigerobo.com>
2024-02-06 15:23:07 +0800

    simplify exllamav2_hf_infer

--- other_infer/exllamav2_hf_infer.py
+++ other_infer/exllamav2_hf_infer.py
@@ -203,8 +203,6 @@ def main(
         max_generate_length: int = 2048
 ):
     print(f"loading model: {model_path}...")
-
-    model = get_model(model_path)
     device = torch.cuda.current_device()
     model, tokenizer, generation_config = get_model(model_path)
 
@@ -213,106 +211,90 @@ def main(
     generation_config.max_new_tokens = max_generate_length
 
     sess_text = ""
+    while True:
+        raw_text = input(
+            'prompt("exit" to end, "clear" to clear session) >>> '
+        )
+        if not raw_text:
+            print("prompt should not be empty!")
+            continue
+        if raw_text.strip() == "exit":
+            print("session ended.")
+            break
+        if raw_text.strip() == "clear":
+            print("session cleared.")
+            sess_text = ""
+            continue
 
-    streamer = TextIteratorStreamer(
-        tokenizer,
-        skip_prompt=True,
-        skip_special_tokens=True,
-        spaces_between_special_tokens=False,
-    )
-    generation_kwargs = generation_config.to_dict()
+        query_text = raw_text.strip()
+        sess_text += tok_ins + query_text
+        input_text = prompt_input.format_map(
+            {"instruction": sess_text.split(tok_ins, 1)[1]}
+        )
+        inputs = tokenizer(
+            input_text,
+            return_tensors="pt",
+            truncation=True,
+            max_length=max_input_length,
+        )
+        inputs = {k: v.to(device) for k, v in inputs.items()}
+        tic = time.perf_counter()
+        print('=' * 100)
+        for text in generate_stream(model, tokenizer, inputs['input_ids'], inputs['attention_mask'],
+                                    generation_config=generation_config):
+            print(text, end='', flush=True)
+        print('')
+        print("=" * 100)
 
-    def eval_generate(**args):
-        with torch.inference_mode(mode=True):
-            model.eval()
-            model.generate(**args)
-
-    with torch.inference_mode(mode=True):
-        model.eval()
-        while True:
-            raw_text = input(
-                'prompt("exit" to end, "clear" to clear session) >>> '
-            )
-            if not raw_text:
-                print("prompt should not be empty!")
-                continue
-            if raw_text.strip() == "exit":
-                print("session ended.")
-                break
-            if raw_text.strip() == "clear":
-                print("session cleared.")
-                sess_text = ""
-                continue
-
-            query_text = raw_text.strip()
-            sess_text += tok_ins + query_text
-            input_text = prompt_input.format_map(
-                {"instruction": sess_text.split(tok_ins, 1)[1]}
-            )
-            inputs = tokenizer(
-                input_text,
-                return_tensors="pt",
-                truncation=True,
-                max_length=max_input_length,
-            )
-            inputs = {k: v.to(device) for k, v in inputs.items()}
-            tic = time.perf_counter()
-            print('=' * 100)
-            for text in generate_stream(model, tokenizer, inputs['input_ids'], inputs['attention_mask'],
-                                        generation_config=generation_config):
-                print(text, end='', flush=True)
-            print('')
-            print("=" * 100)
-
-            #     generation_kwargs["streamer"] = streamer
-            #     for k, v in inputs.items():
-            #         generation_kwargs[k] = v.to(device)
-            #     thread = Thread(
-            #         target=eval_generate, kwargs=generation_kwargs
-            #     )
-            #     thread.start()
-            #     answer = ""
-            #     flag = False
-            #     print("=" * 100)
-            #     for new_text in streamer:
-            #         if new_text.endswith(tokenizer.eos_token):
-            #             new_text = new_text.rsplit(
-            #                 tokenizer.eos_token, 1
-            #             )[0].strip()
-            #             flag = True
-            #         print(new_text, end="")
-            #         answer += new_text
-            #         if flag:
-            #             break
-            #     toc = time.perf_counter()
-            #     num_tok = len(tokenizer.encode(answer))
-            # else:
-            #     if "streamer" in generation_kwargs:
-            #         del generation_kwargs["streamer"]
-            #     inputs = {k: v.to(device) for k, v in inputs.items()}
-            #     output = model.generate(
-            #         **inputs, **generation_config.to_dict()
-            #     )
-            #     toc = time.perf_counter()
-            #     num_tok = output.shape[1]
-            #     output_str = tokenizer.decode(
-            #         output[0],
-            #         skip_special_tokens=False,
-            #         spaces_between_special_tokens=False,
-            #     )
-            #     answer = output_str.rsplit(tok_res, 1)[1].strip()
-            #     if answer.endswith(tokenizer.eos_token):
-            #         answer = answer.rsplit(tokenizer.eos_token, 1)[
-            #             0
-            #         ].strip()
-            #     print(answer)
-            #
-            # sess_text += tok_res + answer
-            res_time = toc - tic
-            print(
-                f"\n[time: {res_time:0.4f} sec, speed: {num_tok / res_time:0.4f} tok/sec]"
-            )
-            print("=" * 100)
+        #     generation_kwargs["streamer"] = streamer
+        #     for k, v in inputs.items():
+        #         generation_kwargs[k] = v.to(device)
+        #     thread = Thread(
+        #         target=eval_generate, kwargs=generation_kwargs
+        #     )
+        #     thread.start()
+        #     answer = ""
+        #     flag = False
+        #     print("=" * 100)
+        #     for new_text in streamer:
+        #         if new_text.endswith(tokenizer.eos_token):
+        #             new_text = new_text.rsplit(
+        #                 tokenizer.eos_token, 1
+        #             )[0].strip()
+        #             flag = True
+        #         print(new_text, end="")
+        #         answer += new_text
+        #         if flag:
+        #             break
+        #     toc = time.perf_counter()
+        #     num_tok = len(tokenizer.encode(answer))
+        # else:
+        #     if "streamer" in generation_kwargs:
+        #         del generation_kwargs["streamer"]
+        #     inputs = {k: v.to(device) for k, v in inputs.items()}
+        #     output = model.generate(
+        #         **inputs, **generation_config.to_dict()
+        #     )
+        #     toc = time.perf_counter()
+        #     num_tok = output.shape[1]
+        #     output_str = tokenizer.decode(
+        #         output[0],
+        #         skip_special_tokens=False,
+        #         spaces_between_special_tokens=False,
+        #     )
+        #     answer = output_str.rsplit(tok_res, 1)[1].strip()
+        #     if answer.endswith(tokenizer.eos_token):
+        #         answer = answer.rsplit(tokenizer.eos_token, 1)[
+        #             0
+        #         ].strip()
+        #     print(answer)
+        #
+        # sess_text += tok_res + answer
+        res_time = toc - tic
+        print(
+            f"\n[time: {res_time:0.4f} sec, speed: {num_tok / res_time:0.4f} tok/sec]"
+        )
+        print("=" * 100)
 
 
 if __name__ == "__main__":
